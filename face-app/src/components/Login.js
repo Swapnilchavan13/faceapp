25cb035adbba860abfc429403083b22545c6179f
swapnil chavan <chavanswapnil822@gmail.com>
2023-06-18 22:41:10 +0530

    update

---
+++ face-app/src/components/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import '../styles/login.module.css';
+
+const Login = ({ handleLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username === 'foo' && password === 'bar') {
+      handleLogin();
+    } else {
+      alert('Invalid credentials');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
